fe0fc4eb81b36f7f2cd3d54669d45cb0286875cd
Alice <alicefrancolisboa@gmail.com>
2026-09-14 21:55:35 -0300

    Align Android Kotlin and Java targets

--- aloha-casa-do-acai/concurso-saltinho/app/build.gradle.kts
+++ aloha-casa-do-acai/concurso-saltinho/app/build.gradle.kts
@@ -3,7 +3,12 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android { namespace = "br.com.concursosaltinho"; compileSdk = 35
     defaultConfig { applicationId = "br.com.concursosaltinho"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0"; buildConfigField("String", "APP_URL", "\"https://concurso-saltinho.onrender.com\"") }
     buildFeatures { buildConfig = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
+kotlin { jvmToolchain(17) }
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
