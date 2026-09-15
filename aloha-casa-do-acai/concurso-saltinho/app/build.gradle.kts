290d69cfad6e51e5847298f31bfd49c32e2aff57
Alice <alicefrancolisboa@gmail.com>
2026-09-14 21:35:48 -0300

    Connect Android app to Render service

--- aloha-casa-do-acai/concurso-saltinho/app/build.gradle.kts
+++ aloha-casa-do-acai/concurso-saltinho/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace = "br.com.concursosaltinho"; compileSdk = 35
-    defaultConfig { applicationId = "br.com.concursosaltinho"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0"; buildConfigField("String", "APP_URL", "\"https://concurso-saltinho-api.onrender.com\"") }
+    defaultConfig { applicationId = "br.com.concursosaltinho"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0"; buildConfigField("String", "APP_URL", "\"https://concurso-saltinho.onrender.com\"") }
     buildFeatures { buildConfig = true }
 }
 dependencies {
